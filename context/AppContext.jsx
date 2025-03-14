75373a90690cf207396238701c102a7f689c3427
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-14 21:45:51 +0200

    Update AppContext.jsx

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,10 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -33,10 +35,21 @@ export const AppContextProvider = (props) => {
         if (user.publicMetadata.role === 'seller') {
             setIsSeller(true)
         }
-        setUserData(userDummyData)
+
+        const token = await getToken()
+
+        const {data} = await axios.get('/api/user/data', { headers: {Authorization: `Bearer ${token}`}})
+
+        if (data.success) {
+            setUserData(data.user)
+            setCartItems(data.user.cartItems)
+        } else {
+            toast.error(data.message)
+        }
+        
 
        } catch (error) {
-        
+        toast.error(error.message)
        }
     }
 
