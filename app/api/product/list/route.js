033e13877a9f94c4e7921f79e71a778645a2d7bf
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-16 17:27:38 +0200

    Update route.js

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -6,11 +6,10 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
     try {
        
-        await connectDB()
 
+        await connectDB()
         const products = await Product.find({})
         return NextResponse.json({success: true, products})
-        
     } catch (error) {
         return NextResponse.json({success: false, message: error.message})
     }
